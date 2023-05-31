53ec6ae60d8c09b89f9fffd1abc45f5ab228e04c
Saddiqq <9adi8.tm@gmail.com>
2023-05-31 15:57:33 +0400

    CRON expression that runs a job at 11:30 PM every Friday the 13th.

--- CronMaster/src/main/java/com/example/CronMaster/schedule.java
+++ CronMaster/src/main/java/com/example/CronMaster/schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class schedule {
-    @Scheduled(cron = "30 11 13 * 5 ")
+    @Scheduled(cron = "0 * 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 * * ")
     public void schedule(){
         System.out.println("Cron Expression Executed");
 
