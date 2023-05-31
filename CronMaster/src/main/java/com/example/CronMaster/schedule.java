fed1e030b2d3c07364e7cd3361b6e0f332964a13
Saddiqq <9adi8.tm@gmail.com>
2023-05-31 16:02:26 +0400

    CRON expression that runs a job every 30 minutes on the 15th and 30th day of every month.

--- CronMaster/src/main/java/com/example/CronMaster/schedule.java
+++ CronMaster/src/main/java/com/example/CronMaster/schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class schedule {
-    @Scheduled(cron = "0 6-20 * * 0-4")
+    @Scheduled(cron = "30 0 15,30 * * ")
     public void schedule(){
         System.out.println("Cron Expression Executed");
 
