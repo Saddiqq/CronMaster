f4c362784d0fba73033837bf0aebcbd0eaf1177f
Saddiqq <9adi8.tm@gmail.com>
2023-05-31 16:00:53 +0400

    CRON expression that runs a job at 10:00 PM every Sunday in the months of July, August, and September.

--- CronMaster/src/main/java/com/example/CronMaster/schedule.java
+++ CronMaster/src/main/java/com/example/CronMaster/schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class schedule {
-    @Scheduled(cron = "0 12 3 * 3")
+    @Scheduled(cron = "0 10 * 7,8,9 0")
     public void schedule(){
         System.out.println("Cron Expression Executed");
 
