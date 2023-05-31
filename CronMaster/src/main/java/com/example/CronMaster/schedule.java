24745a2887527a3976b29f6f204453e96e0a09ca
Saddiqq <9adi8.tm@gmail.com>
2023-05-31 15:56:17 +0400

    CRON expression that runs a job every hour during the first half of the month.

--- CronMaster/src/main/java/com/example/CronMaster/schedule.java
+++ CronMaster/src/main/java/com/example/CronMaster/schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class schedule {
-    @Scheduled(cron = "10 9-18 * * 0-4")
+    @Scheduled(cron = "0 * 1-15 * *")
     public void schedule(){
         System.out.println("Cron Expression Executed");
 
