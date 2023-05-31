cfd408b5be0f99d9ac5d0a2d21168b551f8ce599
Saddiqq <9adi8.tm@gmail.com>
2023-05-31 16:06:00 +0400

    CRON expression that runs a job at 12:00 AM on the first day of every quarter (January, April, July, and October)

--- CronMaster/src/main/java/com/example/CronMaster/schedule.java
+++ CronMaster/src/main/java/com/example/CronMaster/schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class schedule {
-    @Scheduled(cron = "30 16 * 7,8 0-4")
+    @Scheduled(cron = "0 0 * 1,4,7,10 *")
     public void schedule(){
         System.out.println("Cron Expression Executed");
 
