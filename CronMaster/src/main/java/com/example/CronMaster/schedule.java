f7467690666bed48f4000adaef3ebb4e79d52ffc
Saddiqq <9adi8.tm@gmail.com>
2023-05-31 15:54:33 +0400

    CRON expression that runs a job every day at 6:00 PM during daylight saving time (DST) in the United States.

--- CronMaster/src/main/java/com/example/CronMaster/schedule.java
+++ CronMaster/src/main/java/com/example/CronMaster/schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class schedule {
-    @Scheduled(cron = "0 * 15 * *")
+    @Scheduled(cron = " 0 18 * * *")
     public void schedule(){
         System.out.println("Cron Expression Executed");
 
