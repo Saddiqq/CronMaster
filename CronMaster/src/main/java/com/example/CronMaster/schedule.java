7dd6622559f0214f5eacabe0b5c7016e0fa8ce7e
Saddiqq <9adi8.tm@gmail.com>
2023-05-31 16:02:07 +0400

    CRON expression that runs a job every hour from 6:00 AM to 8:00 PM on weekdays.

--- CronMaster/src/main/java/com/example/CronMaster/schedule.java
+++ CronMaster/src/main/java/com/example/CronMaster/schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class schedule {
-    @Scheduled(cron = "15 3 * * * ")
+    @Scheduled(cron = "0 6-20 * * 0-4")
     public void schedule(){
         System.out.println("Cron Expression Executed");
 
